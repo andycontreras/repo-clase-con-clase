d08bf0d6f376fc2f04919a00a4cbf1174dba4d2f
AndyConty <andresconvar@gmail.com>
2021-07-02 04:12:39 -0300

    primer commit

---
+++ index.ts
@@ -0,0 +1,32 @@
+class Album {
+  title:string;
+  songs:string[];
+  constructor (title, songs:string[]){
+    this.title = title;
+    this.songs = songs;
+  }
+}
+
+class Banda {
+  members: string[]
+  albums: Album[]
+  constructor(members:string[], albums:Album[]){
+    this.members = members;
+    this.albums = albums;
+
+  }
+}
+
+function main (){
+  const unAlbum = {
+    title: "Album de marce",
+    songs: ["tu vieja"],
+  };
+  
+  const unaBanda = new Banda(["Andy"], [unAlbum, unAlbum, unAlbum]);
+  // console.log(unaBanda.members)
+  console.log(unaBanda.albums);
+
+}
+
+main()
